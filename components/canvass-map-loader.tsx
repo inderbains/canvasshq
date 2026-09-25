d86e7d77a72df22598b699346f07198f5c0eeb8d
inderbains <125634315+inderbains@users.noreply.github.com>
2026-09-24 23:43:48 -0700

    Add organizationId and districtId props to CanvassMapLoader

--- components/canvass-map-loader.tsx
+++ components/canvass-map-loader.tsx
@@ -19,23 +19,32 @@ const CanvassMap = dynamic(() => import("@/components/canvass-map"), {
 export function CanvassMapLoader({
   addresses,
   campaignId,
+  organizationId,
+  districtId,
   readOnly = false,
   focusAssigned = false,
   assignedTerritories = [],
+  canManageCoverage = false,
 }: {
   addresses: AddressPoint[];
   campaignId: string;
+  organizationId: string;
+  districtId: string;
   readOnly?: boolean;
   focusAssigned?: boolean;
   assignedTerritories?: Feature<Polygon>[];
+  canManageCoverage?: boolean;
 }) {
   return (
     <CanvassMap
       addresses={addresses}
       campaignId={campaignId}
+      organizationId={organizationId}
+      districtId={districtId}
       readOnly={readOnly}
       focusAssigned={focusAssigned}
       assignedTerritories={assignedTerritories}
+      canManageCoverage={canManageCoverage}
     />
   );
 }
